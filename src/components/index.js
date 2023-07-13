456e4d22d2769a18e718aaeb7839c8fb1e4f86f4
juanDev-47 <juan1037av@gmail.com>
2023-07-13 18:18:09 -0500

    new: create pages and components templates

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import Tab from "./Tab";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+
+export { CustomButton, Tab, AIPicker, ColorPicker, FilePicker };
