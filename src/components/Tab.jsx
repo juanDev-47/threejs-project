677712ac5bd7e1e78a41d01494d2b8f2af6f375c
juanDev-47 <juan1037av@gmail.com>
2023-07-25 15:49:10 -0500

    new: create tab component and work in customer.jsx

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -6,7 +6,10 @@ import state from "../store";
 const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
   const snap = useSnapshot(state);
 
-  const activeStyle = isFilterTab && isActiveTab ? { backgroundColor: snap.color, opacity: 0.5 } : { backgroundColor: "transparent", opacity: 1 };
+  const activeStyle =
+    isFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
 
   return (
     <div
@@ -17,7 +20,13 @@ const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
       onClick={handleClick}
       style={activeStyle}
     >
-      <img src={tab.icon} alt={tab.name} className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 H-11/12 object-contain' }`} />
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${
+          isFilterTab ? "w-2/3 h-2/3" : "w-11/12 H-11/12 object-contain"
+        }`}
+      />
     </div>
   );
 };
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -18,6 +18,31 @@ import {
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState("");
+  const [promt, setPromt] = useState("");
+  const [generatingImg, setgeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // show tab content depending on the active tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -30,19 +55,23 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab
-                    key={tab.name}
-                    tab={tab}
-                    handleClick={() => {}}
-
-                  />
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
 
-          <motion.div className="absolute z-10 top-5 right-5" {...fadeAnimation}>
-            <CustomButton title="Go Back" type="filled" handleClick={() => state.intro = true} customStyle="w-fit px-4 py-2.5 font-bold text-sm" />
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton
+              title="Go Back"
+              type="filled"
+              handleClick={() => (state.intro = true)}
+              customStyle="w-fit px-4 py-2.5 font-bold text-sm"
+            />
           </motion.div>
 
           <motion.div className="filtertabs-container">
